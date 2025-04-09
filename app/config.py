76fb657649ccfb9d94de48c59cac4834b60cb607
Fernando Novillo <70916955+Comtex765@users.noreply.github.com>
2025-04-09 13:56:24 -0500

    updated db host

--- app/config.py
+++ app/config.py
@@ -12,5 +12,5 @@ DB_NAME = getenv("DB_NAME")
 
 
 SQLALCHEMY_DATABASE_URI = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}" f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"postgresql://{DB_USER}:{DB_PASSWORD}" f"@[{DB_HOST}]:{DB_PORT}/{DB_NAME}"
 )
